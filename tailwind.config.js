c7fc1c2469aefea1bae5b37d4d4f83bc2299bdcb
Ronny Perez <ronnypdev@gmail.com>
2024-10-08 12:34:05 -0400

    Refactored svgs into icons and implemented color pallette

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        SpaceMono: ["Josefin Sans", "sans-serif"]
+      },
+      colors: {
+        lightGradient: "var(--light-linear-gradient)",
+        darkGradient: "var(--dark-linear-gradient)",
+        primaryBlue: "var(--color-blue)",
+        white: "var(--color-white)",
+        lightGrey: "var(--color-light-grey)",
+        paleGrey: "var(--color-pale-grey)",
+        grey: "var(--color-grey)",
+        midGrey: "var(--color-mid-grey)",
+        darkGrey: "var(--color-dark-grey)",
+        spaceGrey: "var(--color-space-grey)",
+        lightSpaceGrey: "var(--color-light-space-grey)",
+        spaceBlack: "var(--color-space-black)",
+        midnightBlack: "var(--color-midnight-black)"
+      },
+      boxShadow: {
+        "paleWhite": "var(--light-box-shadow)"
+      },
+    },
   },
   plugins: [],
 }
