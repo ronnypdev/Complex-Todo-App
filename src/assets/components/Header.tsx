6aa5f542fcb8dbd76de6ede12cd3157dc60d7a11
Ronny Perez <ronnypdev@gmail.com>
2024-10-23 14:38:16 -0400

    Implemented Header semantic code

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,4 +1,5 @@
 import MoonIcon from "./icons/MoonIcon"
+import OvalIcon from "./icons/OvalIcon"
 
 export default function Header() {
   return (
@@ -6,12 +7,22 @@ export default function Header() {
       <header className="bg-hero-desktop-light h-[330px] bg-cover bg-center bg-no-repeat">
         <div className="max-container h-full">
           <div>
-            <h1>Header Component</h1>
-            <MoonIcon
-              fillColor="#FFFFFF"
-              stroke="#FFFFFF"
-              hoverState="hover:fill-white cursor-pointer"
-            />
+            <div className="list-header">
+              <h1>Header Component</h1>
+              <MoonIcon
+                fillColor="#FFFFFF"
+                stroke="#FFFFFF"
+                hoverState="hover:fill-white cursor-pointer"
+              />
+            </div>
+            <label htmlFor="listItem">
+              <OvalIcon
+                fillColor="#E3E4F1"
+                stroke="#E3E4F1"
+                hoverState="hover:fill-lightGrey cursor-pointer"
+              />
+              <input type="text" name="listItem" id="listItem" placeholder="Create a new todo…"/>
+            </label>
           </div>
         </div>
       </header>
