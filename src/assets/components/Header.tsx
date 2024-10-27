32b8ebd3f8f14dba20cf7d538d67b7b8e21dbb89
Ronny Perez <ronnypdev@gmail.com>
2024-10-27 17:40:30 -0400

    Implemented and completed the Header component

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -6,23 +6,20 @@ export default function Header() {
     <>
       <header className="bg-hero-desktop-light h-[330px] bg-cover bg-center bg-no-repeat">
         <div className="max-container h-full">
-          <div>
-            <div className="list-header">
-              <h1>Header Component</h1>
-              <MoonIcon
-                fillColor="#FFFFFF"
-                stroke="#FFFFFF"
-                hoverState="hover:fill-white cursor-pointer"
-              />
+          <div className="flex justify-center items-center h-full">
+            <div className="w-[540px] my-0 mx-auto">
+              <div className="list-header flex justify-between items-center mb-10">
+                <h1 className="text-white text-[40px] leading-normal uppercase font-bold style not-italic tracking-[15px]">todo</h1>
+                <MoonIcon
+                  fillColor="#FFFFFF"
+                  hoverState="hover:fill-white cursor-pointer"
+                />
+              </div>
+              <label htmlFor="listItem" className="relative">
+                <OvalIcon />
+                <input className="w-full max-w-full py-[23px] pr-5 pl-[47px] shadow-paleWhite rounded-[5px] placeholder:text-darkGrey" type="text" name="listItem" id="listItem" placeholder="Create a new todo item..."/>
+              </label>
             </div>
-            <label htmlFor="listItem">
-              <OvalIcon
-                fillColor="#E3E4F1"
-                stroke="#E3E4F1"
-                hoverState="hover:fill-lightGrey cursor-pointer"
-              />
-              <input type="text" name="listItem" id="listItem" placeholder="Create a new todo…"/>
-            </label>
           </div>
         </div>
       </header>
--- src/assets/components/icons/OvalIcon.tsx
+++ src/assets/components/icons/OvalIcon.tsx
@@ -1,19 +1,4 @@
-type iconProps = {
-  fillColor: string;
-  stroke?: string;
-  hoverState: string;
-}
+export default function OvalIcon() {
+  return <div className="oval-icon"></div>
 
-export default function OvalIcon({
-  fillColor,
-  stroke,
-  hoverState,
-}: iconProps) {
-  return (
-    <>
-      <svg width="25" height="24" viewBox="0 0 25 24" fill={fillColor} xmlns="http://www.w3.org/2000/svg" className={hoverState}>
-        <circle id="Oval Copy" cx="12" cy="12" r="12" fill={fillColor} stroke={stroke} />
-      </svg>
-    </>
-  )
 }
